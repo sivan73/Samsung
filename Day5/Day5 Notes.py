2bafb4d9ca0f76e8fcd743e123d8816bbc32acab
sivan73 <149456197+sivan73@users.noreply.github.com>
2026-06-19 11:40:00 +0530

    Day5S1,2

---
+++ Day5/Day5 Notes.py
@@ -0,0 +1,63 @@
+sorting
+    organizing into monotonic sequence
+    orderable, define < operator, = operator
+
+non-indexable - i.e linkedlist
+    Bubble sort
+
+indexable - array/list(python)
+
+
+arr = random.sample(range(0,13),11)
+
+#Bubble sort - Mutating only (inplace)
+
+def bubble(a,left,right):
+    for i in range(left,right-1):
+        if a[i] > a[i+1] : a[i],a[i+1] = a[i+1],a[i]
+
+def bubblesort(a):
+    for i in range(len(a)) : bubble(a,0,len(a)-i)
+
+
+#Selection sort - Mutating only(inplace)
+
+                find min/max
+                swap if greater/lesser/ wrt current location
+                O(n*n)
+
+def selectionsort(x,compare):
+    for i in range(len(x)):
+        loc = find_element(x,i,len(x),compare)
+        if compare(x[loc],x[i]) : x[loc],x[i] = x[i],x[loc]
+
+        
+
+def selectionsort(x,compare):
+    for i in range(len(x)):
+        loc = find_element(x,i,len(x),compare)
+        if loc!= : x[loc],x[i] = x[i],x[loc]
+
+
+
+def find_element(x,left,right,compare):
+    curr,val = left,x[left]
+    for i in range(left,right):
+        if compare(x[curr],x[i]): curr,val = i,x[i]
+    return curr
+
+
+#Insertion sort 
+
+def insertionsort(a):
+    if a==[] : return []
+    b = [a.pop()]
+    while a != []:
+        i = 0
+        while i < len(b) and b[i]<a[-1]:
+            i+= 1
+        b.insert(i,a.pop())
+        print(a,b)
+    return b
+
+
